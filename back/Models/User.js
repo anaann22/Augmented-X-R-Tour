969c636efebe2d3c96f043ccdeb014e778e0c662
anaann22 <ana.burlacu00@e-uvt.ro>
2024-02-27 20:02:44 +0200

    model user

---
+++ back/Models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true, 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+    },
+    
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: false,
+        default: false, 
+    },
+    avatarUrl: String, 
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model('User', UserSchema);
--- back/index.js
+++ back/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 const app = express();
 
@@ -6,9 +8,17 @@ app.get('/', (req,res) => {
     res.send('Hello');
 });
 
-app.listen(4444, (err) => {
-    if(err) {
-        return console.log(err);
+app.use(express.json());
+
+mongoose
+  .connect('mongodb+srv://ana:K9Wi9CNddY1iAl0x@cluster0.yivkgdq.mongodb.net/Augmented-XR-Tour?retryWrites=true&w=majority')
+  .then(() => console.log('DB OK'))
+  .catch((err) => console.log('DB ERROR', err));
+
+  app.listen(8080, (err) => {
+    if (err) {
+      return console.log(err);
     }
-    console.log('Ok');
-});
+    console.log('Server ok');
+  });
+
