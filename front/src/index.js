05b8a781728e4b1d41cc68da348d23f891761c25
anaann22 <ana.burlacu00@e-uvt.ro>
2024-03-03 17:59:04 +0200

    delete axios

--- front/src/index.js
+++ front/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const axios = require('axios');
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,12 +12,5 @@ root.render(
   </React.StrictMode>
 );
 
-axios.get('http://localhost:8080')
-  .then(response => {
-    console.log(response.data); // This will log the response from your frontend
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
 
 reportWebVitals();
